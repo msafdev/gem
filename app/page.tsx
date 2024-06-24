857f82cf1c52689cbf433eb21e1395fd7f8e24e4
salman261002 <salmanalfarisi261002@gmail.com>
2024-06-24 23:30:43 +0700

    feat: update button

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Badge from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Gem, Gamepad } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
       <Badge icon={<Gamepad className="h-3.5 w-3.5" />}>
         1 game baru ditambahkan
       </Badge>
-      <h1 className="max-w-xl text-balance text-center text-2xl font-bold md:text-4xl mt-3">
+      <h1 className="mt-3 max-w-xl text-balance text-center text-2xl font-bold md:text-4xl">
         Kumpulan game asik buat kamu yang lagi gabut
       </h1>
       <p className="mt-3 max-w-xl text-balance text-center text-sm text-muted-foreground md:text-base">
@@ -16,12 +17,19 @@ export default function Home() {
         ribet!
       </p>
       <div className="mt-5 flex flex-wrap items-center gap-x-3">
-        <Button size="sm" className="">
-          Kontribusi
+        <Button size="sm" className="" asChild>
+          <Link href={"https://github.com/msafdev/gem"} target="_blank">
+            Kontribusi
+          </Link>
         </Button>
-        <Button size="sm" className="gap-x-2 bg-red-600 hover:bg-red-500 text-white">
-          <Gem className="h-4 w-4" />
-          Trakteer
+        <Button
+          size="sm"
+          className="gap-x-2 bg-red-600 text-white hover:bg-red-500"
+        >
+          <Link href={"https://trakteer.com/msafdev"} target="_blank">
+            <Gem className="h-4 w-4" />
+            Trakteer
+          </Link>
         </Button>
       </div>
     </main>
